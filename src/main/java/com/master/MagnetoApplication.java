f45eeb9d647df741c7811484f6c55134045c1236
Mr.cheng <chengzhikang2015@163.com>
2017-04-25 22:58:10 +0800

    1.0

--- src/main/java/com/master/MagnetoApplication.java
+++ src/main/java/com/master/MagnetoApplication.java
@@ -3,6 +3,7 @@ package com.master;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test
 @SpringBootApplication
 public class MagnetoApplication {
 
